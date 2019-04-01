cc60d7ba574900e9f2e538bed3bdc1b28725a5fb
Grigory Chirkov <gchirkov@princeton.edu>
2019-04-01 17:31:46 -0400

    add axi4 mem interface for vc707

--- piton/design/chipset/mc/rtl/mc_top.v
+++ piton/design/chipset/mc/rtl/mc_top.v
@@ -229,7 +229,7 @@ noc_bidir_afifo  mig_afifo  (
 );
 
 
-`ifndef AXI4_MEM_INTF
+`ifndef PITONSYS_AXI4_MEM
 noc_mig_bridge    #  (
     .MIG_APP_ADDR_WIDTH (`MIG_APP_ADDR_WIDTH        ),
     .MIG_APP_DATA_WIDTH (`MIG_APP_DATA_WIDTH        )
@@ -360,7 +360,7 @@ mig_7series_0   mig_7series_0 (
     .sys_rst                        (sys_rst_n)
 );
 
-`else // ifdef AXI4_MEM_INTF, use AXI, works only with VC707
+`else // ifdef AXI4_MEM, use AXI, works only with VC707
 
     `define C_M_AXI4_DATA_WIDTH  512
     `define C_M_AXI4_ID_WIDTH    16
@@ -450,6 +450,13 @@ mig_7series_0   mig_7series_0 (
         .m_axi_awaddr(m_axi_awaddr),
         .m_axi_awlen(m_axi_awlen),
         .m_axi_awsize(m_axi_awsize),
+        .m_axi_awburst(m_axi_awburst),
+        .m_axi_awlock(m_axi_awlock),
+        .m_axi_awcache(m_axi_awcache),
+        .m_axi_awprot(m_axi_awprot),
+        .m_axi_awqos(m_axi_awqos),
+        .m_axi_awregion(m_axi_awregion),
+        .m_axi_awuser(m_axi_awuser),
         .m_axi_awvalid(m_axi_awvalid),
         .m_axi_awready(m_axi_awready),
 
@@ -457,11 +464,13 @@ mig_7series_0   mig_7series_0 (
         .m_axi_wdata(m_axi_wdata),
         .m_axi_wstrb(m_axi_wstrb),
         .m_axi_wlast(m_axi_wlast),
+        .m_axi_wuser(m_axi_wuser),
         .m_axi_wvalid(m_axi_wvalid),
         .m_axi_wready(m_axi_wready),
 
         .m_axi_bid(m_axi_bid),
         .m_axi_bresp(m_axi_bresp),
+        .m_axi_buser(m_axi_buser),
         .m_axi_bvalid(m_axi_bvalid),
         .m_axi_bready(m_axi_bready),
 
@@ -469,6 +478,13 @@ mig_7series_0   mig_7series_0 (
         .m_axi_araddr(m_axi_araddr),
         .m_axi_arlen(m_axi_arlen),
         .m_axi_arsize(m_axi_arsize),
+        .m_axi_arburst(m_axi_arburst),
+        .m_axi_arlock(m_axi_arlock),
+        .m_axi_arcache(m_axi_arcache),
+        .m_axi_arprot(m_axi_arprot),
+        .m_axi_arqos(m_axi_arqos),
+        .m_axi_arregion(m_axi_arregion),
+        .m_axi_aruser(m_axi_aruser),
         .m_axi_arvalid(m_axi_arvalid),
         .m_axi_arready(m_axi_arready),
 
@@ -476,12 +492,13 @@ mig_7series_0   mig_7series_0 (
         .m_axi_rdata(m_axi_rdata),
         .m_axi_rresp(m_axi_rresp),
         .m_axi_rlast(m_axi_rlast),
+        .m_axi_ruser(m_axi_ruser),
         .m_axi_rvalid(m_axi_rvalid),
         .m_axi_rready(m_axi_rready)
 
     );
 
-        mig_7series_0 u_mig_7series_0 (
+    mig_7series_axi4 u_mig_7series_axi4 (
 
         // Memory interface ports
         .ddr3_addr                      (ddr_addr),  // output [13:0]      ddr3_addr
@@ -506,7 +523,7 @@ mig_7series_0   mig_7series_0 (
         .ui_clk                         (ui_clk),  // output            ui_clk
         .ui_clk_sync_rst                (ui_clk_sync_rst),  // output           ui_clk_sync_rst
         .mmcm_locked                    (),  // output           mmcm_locked
-        .aresetn                        (aresetn),  // input            aresetn
+        .aresetn                        (sys_rst_n),  // input            aresetn
         .app_sr_req                     (app_sr_req),  // input         app_sr_req
         .app_ref_req                    (app_ref_req),  // input            app_ref_req
         .app_zq_req                     (app_zq_req),  // input         app_zq_req
