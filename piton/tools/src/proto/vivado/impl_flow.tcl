e712883e8e08fd11b5e4a67fc86f72fdda60a5a5
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2019-05-30 20:25:22 +0200

    Bump Ariane and adjust paths, increase timing otpimization effort in FPGA flow

--- piton/tools/src/proto/vivado/impl_flow.tcl
+++ piton/tools/src/proto/vivado/impl_flow.tcl
@@ -1,6 +1,6 @@
 # Copyright (c) 2016 Princeton University
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #     * Redistributions of source code must retain the above copyright
@@ -11,7 +11,7 @@
 #     * Neither the name of Princeton University nor the
 #       names of its contributors may be used to endorse or promote products
 #       derived from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY PRINCETON UNIVERSITY "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -44,6 +44,10 @@ open_project ${VIVADO_PROJECT_FILE}
 # Update Verilog MACROs property
 set_property verilog_define ${ALL_VERILOG_MACROS} [get_fileset sources_1]
 set_property verilog_define ${ALL_VERILOG_MACROS} [get_fileset sim_1]
+
+# Some additional effort to meet timing
+set_property flow {Vivado Implementation 2016} [get_runs impl_1]
+set_property strategy Performance_ExtraTimingOpt [get_runs impl_1]
 
 # Dealing with Vivado case, when it locks IPs as old ones
 upgrade_ip [get_ips -all]
