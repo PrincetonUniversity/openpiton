f94bad271927825cfeece5510cbb7193b1ad06d0
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2019-06-04 20:54:57 +0200

    Adapt fpga_lib and pitonstream to select correct segments from storage_addr_trans.tmp.v

--- piton/tools/src/proto/fpga_lib.py
+++ piton/tools/src/proto/fpga_lib.py
@@ -175,20 +175,29 @@ def calcUARTLatch(design_data, board):
 # Name:     isTranslatorOK
 # Input:    addr_data_map   -   addr:data map for a test to check
 #           flog            -   file descriptor for loggin
+#           ariane          -   if true, this will only consider the first
+#                               entry. if flase, the first entry will be skipped
 # Output:   True
-# Description: Tests if $DV_ROOT/chipset/rtl/storage_addr_trans.v
+# Description: Tests if $DV_ROOT/chipset/rtl/storage_addr_trans.tmp.v
 #              can be used for mapping of addr:map address
 ############################################################################
-def isTranslatorOK(addr_data_map, flog):
-    map_loc = DV_ROOT + "/design/chipset/rtl/storage_addr_trans_unified.v"
+def isTranslatorOK(addr_data_map, flog, ariane):
+    map_loc = DV_ROOT + "/design/chipset/rtl/storage_addr_trans_unified.tmp.v"
     fname = os.path.join(map_loc)
     f = open(fname, 'r')
 
     trans_sections = list()
+    cnt = 0
     for line in f:
         m = re.search(r"in_section.*>=\s+64'h([0-9a-fA-F]+).*<\s+64'h([0-9a-fA-F]+)", line)
         if m != None:
-            trans_sections.append((int(m.group(1), 16), int(m.group(2), 16)))
+            if ariane:
+                if cnt == 0:
+                    trans_sections.append((int(m.group(1), 16), int(m.group(2), 16)))
+            else:
+                if cnt > 0:
+                    trans_sections.append((int(m.group(1), 16), int(m.group(2), 16)))
+            cnt+=1
 
     f.close()
 
