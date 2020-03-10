0cab624ac9fc5bdf4c460aba008c3c9767570709
Fei Gao <feig@princeton.edu>
2020-03-09 23:49:12 -0400

    Use L2_SEND_NC_REQ to enable off-chip NC reqs. Fixed a bug related to CSM

--- piton/design/chip/tile/l15/rtl/noc1encoder.v
+++ piton/design/chip/tile/l15/rtl/noc1encoder.v
@@ -440,9 +440,11 @@ begin
       `PCX_SZ_16B:
          msg_options_2[`MSG_DATA_SIZE_] = `MSG_DATA_SIZE_16B;
    endcase
+`ifdef L2_SEND_NC_REQ
    if (req_type == `L15_NOC1_REQTYPE_IFILL_REQUEST) begin
         msg_options_2[`MSG_DATA_SIZE_] = `MSG_DATA_SIZE_64B;
    end
+`endif
 
    msg_options_2[`MSG_CACHE_TYPE_] = msg_cache_type;
    msg_options_2[`MSG_SUBLINE_VECTOR_] = msg_subline_vector;
