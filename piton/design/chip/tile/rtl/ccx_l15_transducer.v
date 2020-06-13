98ae92c22d16d5eb60fe0ee009dbd033451d3875
Jonathan Balkind <jbalkind@cs.princeton.edu>
2020-06-13 11:50:02 -0400

    Add missing includes to ccx_l15_transducer

--- piton/design/chip/tile/rtl/ccx_l15_transducer.v
+++ piton/design/chip/tile/rtl/ccx_l15_transducer.v
@@ -24,6 +24,9 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+`include "l15.tmp.h"
+`include "iop.h"
+
 module ccx_l15_transducer (
     input                           clk,
     input                           rst_n,
