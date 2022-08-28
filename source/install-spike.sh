61a5b0f4226225c34e62587273a8ea17f2c4a48e
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2022-08-28 15:52:08 +0000

    Create install-spike.sh

---
+++ source/install-spike.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+
+cd $ROOT/tmp
+
+if [ -z ${NUM_JOBS} ]; then
+    NUM_JOBS=1
+fi
+
+if [ ! -e "${RISCV}/bin/spike"  ]; then
+    echo "Installing Spike"
+    git clone https://github.com/riscv/riscv-isa-sim.git 
+    cd riscv-isa-sim
+    mkdir -p build
+    cd build
+    ../configure --prefix="$RISCV/"
+    make -j${NUM_JOBS}
+    make install
+else
+    echo "Using Spike from cached directory."
+fi
