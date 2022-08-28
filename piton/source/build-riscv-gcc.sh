98578268eb5f09d00df266140af487eb2a389746
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2022-08-28 15:46:57 +0000

    Create build-riscv-gcc.sh

---
+++ piton/source/build-riscv-gcc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# call with first argument = 0 to checkout only
+
+set -e
+ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+
+mkdir -p $RISCV
+
+cd $RISCV
+
+if [ -z ${NUM_JOBS} ]; then
+    NUM_JOBS=1
+fi
+
+
+if ! [ -e $RISCV/bin ]; then
+
+    cd /home/$USER/riscv-gnu-toolchain
+
+    if [[ $1 -ne "0" || -z ${1} ]]; then
+      echo "Compiling RISC-V Toolchain"
+      ./configure --prefix=$RISCV > /dev/null
+      make -j${NUM_JOBS} > /dev/null
+      echo "Compilation Finished"
+    fi
+fi
