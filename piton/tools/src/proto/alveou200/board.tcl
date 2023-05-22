3f996a57835b8c12cf7105cf9ec49bab6bef9092
Daniel J.Mazure <dasjimaz@gmail.com>
2023-05-22 14:55:21 -0700

    Don't include the extension when importing bd files

--- piton/tools/src/proto/alveou200/board.tcl
+++ piton/tools/src/proto/alveou200/board.tcl
@@ -39,6 +39,6 @@ set BOARD_DEFAULT_VERILOG_MACROS "ALVEO_BOARD"
 source $DV_ROOT/tools/src/proto/${BOARD}/meep_shell.tcl
 
 # Grab the file from where the above tcl script has placed it
-set DESIGN_BD_FILES [list $DV_ROOT/design/chipset/xilinx/alveou200/meep_shell.bd]
+set DESIGN_BD_FILES [list $DV_ROOT/design/chipset/xilinx/alveou200/meep_shell]
 
 
