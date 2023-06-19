1887cc574c34a12bc3f785565b77964453200b09
AileonN <noeliaoliete1a@gmail.com>
2023-06-19 15:50:58 +0200

    Support for building the HPDC files

--- piton/piton_settings.bash
+++ piton/piton_settings.bash
@@ -12,6 +12,7 @@ export DV_ROOT=$PITON_ROOT/piton
 export MODEL_DIR=$PITON_ROOT/build
 export OST1_ROOT=$DV_ROOT/design/chip/tile/sparc
 export ARIANE_ROOT=$DV_ROOT/design/chip/tile/ariane
+export HPDCACHE_DIR=$ARIANE_ROOT/cv-hpdcache
 
 if [ `uname -s` = "SunOS" ]
 then
