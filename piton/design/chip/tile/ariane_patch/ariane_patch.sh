954731356e84372e84b9f7247a135cfb6153091e
Alex Kropotov <alexander.kropotov@bsc.es>
2024-09-29 13:58:59 +0200

    #TestCICD Merge branch 'production_origCVA6' into 'production'

--- piton/design/chip/tile/ariane_patch/ariane_patch.sh
+++ piton/design/chip/tile/ariane_patch/ariane_patch.sh
@@ -1,5 +1,4 @@
-# Modified by Barcelona Supercomputing Center on October, 2023
 
 echo "Patching Ariane submodule..."
-cp $DV_ROOT/design/chip/tile/ariane_patch/Flist.ariane $ARIANE_ROOT
-# cp $DV_ROOT/design/chip/tile/ariane_patch/frontend.sv  $ARIANE_ROOT/src/frontend
+cd $ARIANE_ROOT
+git apply $DV_ROOT/design/chip/tile/ariane_patch/ariane.patch
