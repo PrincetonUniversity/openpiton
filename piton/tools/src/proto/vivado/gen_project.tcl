b9062cdaa0fa9161e43a35f17e8351012b4b5b8f
Alexander Kropotov <alexander.kropotov@bsc.es>
2025-03-13 00:03:43 +0100

    Insertion of Xilinx AXI-Stream interconnect (single-channel CDC configuration) in-between of each NOC inter-tile connection.

--- piton/tools/src/proto/vivado/gen_project.tcl
+++ piton/tools/src/proto/vivado/gen_project.tcl
@@ -153,6 +153,10 @@ if { $BOARD_DEFAULT_VERILOG_MACROS == "ALVEO_BOARD" } {
   # (to save BD: write_bd_tcl -force -no_project_wrapper ../piton/design/chipset/meep/jtag_shell.tcl)
   source $DV_ROOT/design/chipset/meep/jtag_shell.tcl
 
+  # Generating NOCs muxer/demuxer
+  source $DV_ROOT/design/chipset/meep/nocs_mux_shell.tcl
+  source $DV_ROOT/design/chipset/meep/nocs_demux_shell.tcl
+
   # Generating Ethernet system
   if {[info exists ::env(PROTOSYN_RUNTIME_BOARD)]} {
     set g_board_part [string map {"alveo" ""} $::env(PROTOSYN_RUNTIME_BOARD)]
@@ -178,17 +182,6 @@ if { $BOARD_DEFAULT_VERILOG_MACROS == "ALVEO_BOARD" } {
   # source $DV_ROOT/design/chipset/io_ctrl/xilinx/common/ip_cores/eth_cmac_syst/tcl/gen_project.tcl
   source $DV_ROOT/design/chipset/io_ctrl/xilinx/common/ip_cores/eth_cmac_syst/tcl/eth_cmac_syst.tcl
   cr_bd_Eth_CMAC_syst ""
-
-  # Create IPs of Xilix AXI-stream interconnect (axis_muxer with True Round-Robin arbitration of NOC packets)
-  # create_ip -vendor xilinx.com -library ip -name axis_interconnect -version 2.1 -module_name axis_muxer
-  # set_property -dict [list CONFIG.NUM_MI                                       {1}] [get_ips axis_muxer]
-  # set_property -dict [list CONFIG.NUM_SI                 {$env(PITON_NUM_TILES)*3}] [get_ips axis_muxer]
-  # set_property -dict [list CONFIG.ARB_ON_TLAST                                 {1}] [get_ips axis_muxer]
-  # set_property -dict [list CONFIG.ARB_ON_MAX_XFERS                             {0}] [get_ips axis_muxer]
-  # set_property -dict [list CONFIG.ARB_ALGORITHM                                {3}] [get_ips axis_muxer]
-
-  # create_ip -vendor xilinx.com -library ip -name axis_interconnect -version 2.1 -module_name axis_demuxer
-  # set_property -dict [list CONFIG.NUM_MI                 {$env(PITON_NUM_TILES)*3}] [get_ips axis_demuxer]
 }
 
 # Set 'sources_1' fileset file properties for local files
