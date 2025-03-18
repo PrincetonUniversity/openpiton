23c2eaf13814ad12ef11b974d306d317fca3f7ad
Alexander Kropotov <alexander.kropotov@bsc.es>
2025-03-18 07:24:49 +0100

    Number of muxed-demuxed NOC channels is extended to `3*PITON_NUM_TILES`, thus giving 12 channels per horizont and 12 channels per vertical on 2x2 tested configuration (2 instances of 12-channel muxer-demuxer).

--- piton/design/chipset/meep/nocs_demux_shell.tcl
+++ piton/design/chipset/meep/nocs_demux_shell.tcl
@@ -142,8 +142,7 @@ current_bd_design $design_name
   # Set parent object as current
   current_bd_instance $parentObj
 
-#   set NOC_CHANS [expr $::env(PITON_NUM_TILES) * 3]
-  set NOC_CHANS 3
+  set NOC_CHANS [expr $::env(PITON_NUM_TILES) * 3]
 
   # Create IPs of Xilix AXI-stream interconnect
   set axis_demuxer [ create_bd_cell -type ip -vlnv xilinx.com:ip:axis_interconnect:2.1 axis_demuxer]
@@ -164,7 +163,7 @@ current_bd_design $design_name
   connect_bd_net [get_bd_ports xbar_rstn] [get_bd_pins axis_demuxer/S00_AXIS_ARESETN]
 
   create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:axis_rtl:1.0                                                s_axis
-  # in TCL we don't have explicit log2(), so using just $NOC_CHANS instead of sufficient its logarithm
+  # in TCL we don't have explicit log2(), so using just $NOC_CHANS instead of enough its logarithm
   set_property -dict [list CONFIG.HAS_TLAST 1 CONFIG.TDATA_NUM_BYTES 8 CONFIG.TDEST_WIDTH $NOC_CHANS] [get_bd_intf_ports s_axis]
   connect_bd_intf_net [get_bd_intf_pins axis_demuxer/S00_AXIS]                                        [get_bd_intf_ports s_axis]
 
--- piton/design/chipset/meep/nocs_mux_shell.tcl
+++ piton/design/chipset/meep/nocs_mux_shell.tcl
@@ -142,8 +142,7 @@ current_bd_design $design_name
   # Set parent object as current
   current_bd_instance $parentObj
 
-#   set NOC_CHANS [expr $::env(PITON_NUM_TILES) * 3]
-  set NOC_CHANS 3
+  set NOC_CHANS [expr $::env(PITON_NUM_TILES) * 3]
 
   # Create IPs of Xilix AXI-stream interconnect (axis_muxer with True Round-Robin arbitration of NOC packets)
   set axis_muxer [ create_bd_cell -type ip -vlnv xilinx.com:ip:axis_interconnect:2.1 axis_muxer]
@@ -186,7 +185,7 @@ current_bd_design $design_name
 
     connect_bd_net [get_bd_pins gndx1/dout] [get_bd_pins axis_muxer/S[format {%02d} $idx]_ARB_REQ_SUPPRESS]
     create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:axis_rtl:1.0                                                s_axis_$idx
-    # in TCL we don't have explicit log2(), so using just $NOC_CHANS instead of sufficient its logarithm
+    # in TCL we don't have explicit log2(), so using just $NOC_CHANS instead of enough its logarithm
     set_property -dict [list CONFIG.HAS_TLAST 1 CONFIG.TDATA_NUM_BYTES 8 CONFIG.TDEST_WIDTH $NOC_CHANS] [get_bd_intf_ports s_axis_$idx]
     connect_bd_intf_net [get_bd_intf_pins axis_muxer/S[format {%02d} $idx]_AXIS] [get_bd_intf_ports                        s_axis_$idx]
 
