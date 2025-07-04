b72c5bf2a2639ce703c12780617dc488887c97a1
Alexander Kropotov <alexander.kropotov@bsc.es>
2025-07-04 12:15:16 +0200

    Moving AXI `tdest` signals from `chip.sv.pyv` to `axistx_aurora_bridge.tcl` since they don't need to be flexible but fixed to a port number.

--- piton/design/chipset/meep/axistx_aurora_bridge.tcl
+++ piton/design/chipset/meep/axistx_aurora_bridge.tcl
@@ -265,16 +265,25 @@ current_bd_design $design_name
     set in_fifo_$idx [create_bd_cell -type ip -vlnv xilinx.com:ip:axis_data_fifo:2.0 in_fifo_$idx]
     set_property -dict [list \
       CONFIG.FIFO_DEPTH {16} \
+      CONFIG.TDEST_WIDTH.VALUE_SRC USER \
+      CONFIG.TDEST_WIDTH 8 \
     ] [get_bd_cells in_fifo_$idx]
     connect_bd_net [get_bd_ports sys_clk]  [get_bd_pins in_fifo_$idx/s_axis_aclk]
     connect_bd_net [get_bd_ports sys_rstn] [get_bd_pins in_fifo_$idx/s_axis_aresetn]
 
     # make_bd_intf_pins_external [get_bd_intf_pins axis_muxer/S[format {%02d} $idx]_AXIS]
     # set_property name "s_axis${idx}" [get_bd_intf_ports S[format {%02d} $idx]_AXIS_0]
-    create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:axis_rtl:1.0                                                     s_axis_$idx
-    set_property -dict [list CONFIG.HAS_TLAST 1 CONFIG.TDATA_NUM_BYTES $AXIS_AUR_BYTES CONFIG.TDEST_WIDTH 8] [get_bd_intf_ports s_axis_$idx]
-    connect_bd_intf_net [get_bd_intf_pins in_fifo_$idx/S_AXIS] [get_bd_intf_ports                                               s_axis_$idx]
+    create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:axis_rtl:1.0                                s_axis_$idx
+    set_property -dict [list CONFIG.HAS_TLAST 1 CONFIG.TDATA_NUM_BYTES $AXIS_AUR_BYTES] [get_bd_intf_ports s_axis_$idx]
+    connect_bd_intf_net [get_bd_intf_pins in_fifo_$idx/S_AXIS] [get_bd_intf_ports                          s_axis_$idx]
     connect_bd_intf_net [get_bd_intf_pins in_fifo_$idx/M_AXIS] [get_bd_intf_pins axis_muxer/S[format {%02d} $idx]_AXIS]
+
+    set in_dest_$idx [create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 in_dest_$idx]
+    set_property -dict [ list \
+      CONFIG.CONST_WIDTH {8} \
+      CONFIG.CONST_VAL $idx \
+    ] [get_bd_cells in_dest_$idx]
+    connect_bd_net [get_bd_pins in_dest_$idx/dout] [get_bd_pins in_fifo_$idx/s_axis_tdest]
 
     set inrdy_rst_$idx [ create_bd_cell -type ip -vlnv xilinx.com:ip:util_vector_logic:2.0 inrdy_rst_$idx ]
     set_property -dict [list \
