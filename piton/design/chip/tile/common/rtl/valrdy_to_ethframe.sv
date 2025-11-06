595d76f5533acffd41346132a5723fee7899e86e
Alexander Kropotov <alexander.kropotov@bsc.es>
2025-11-06 15:24:04 +0100

    For debug relaxing the check of recieved ACK packet on Tx side: only DST/SRC MAC addr and Eth type, without Eth frame ID.

--- piton/design/chip/tile/common/rtl/valrdy_to_ethframe.sv
+++ piton/design/chip/tile/common/rtl/valrdy_to_ethframe.sv
@@ -107,7 +107,7 @@ always @(posedge clk)
   end
 
 assign ready_in = ready_out && !hdr_tx_cnt;
-assign valid_out = valid_in; // && !wait_ack;
+assign valid_out = valid_in && !wait_ack;
 
 reg [`MSG_LENGTH_WIDTH-1:0] remaining_flits;
 always @(posedge clk)
@@ -130,8 +130,8 @@ wire [ETH_PAYLD_LEN_WIDTH-1:0] ethfr_payld_len_rx = {header_rx[2*MAC_ADDR_WIDTH 
                                                      header_rx[2*MAC_ADDR_WIDTH+7 +: 7]};
 assign header_ok = (header_rx[2*MAC_ADDR_WIDTH-1 :0] == dst_src_mac_ref) &&
                    //(ethfr_payld_len_rx == MIN_ETHFR_PAYLD_LEN) && // for IEEE802.3 usage of Ethertype field as Eth payload length
-                   (header_rx[2*MAC_ADDR_WIDTH +: ETH_PAYLD_LEN_WIDTH] == {ETHTYPE_BYTE0,ETHTYPE_BYTE1}) && // checking the custom Ethertype
-                   (header_rx[ETHHDR_WIDTH +: ETHFR_ID_WIDTH] == (ethfr_id-'h1));
+                   (header_rx[2*MAC_ADDR_WIDTH +: ETH_PAYLD_LEN_WIDTH] == {ETHTYPE_BYTE0,ETHTYPE_BYTE1}); // && // checking the custom Ethertype
+                  //  (header_rx[ETHHDR_WIDTH +: ETHFR_ID_WIDTH] == (ethfr_id-'h1));
 assign ready_ack = 1'b1;
 
 always @(posedge clk)
@@ -148,6 +148,8 @@ always @(posedge clk)
       end
       if (last_ack) hdr_rx_cnt <= ETHHDR_NOC_FLITS;
     end
+    // Giving a chance just for one clock cycle to treat header (by header_ok) before clearing it
+    else if (hdr_rx_cnt == ETHHDR_NOC_FLITS) header_rx <= 'h0;
   end
 
 endmodule
