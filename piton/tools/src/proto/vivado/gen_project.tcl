b5eb8bb774d45df1a712039ac97a34a56d36b711
Alexander Kropotov <alexander.kropotov@bsc.es>
2026-01-27 08:37:25 +0100

    Fix moving the loop over NOCs inside "FROM/TO" branches, accordingly with correction of required channel offset.

--- piton/tools/src/proto/vivado/gen_project.tcl
+++ piton/tools/src/proto/vivado/gen_project.tcl
@@ -210,11 +210,11 @@ if { $BOARD_DEFAULT_VERILOG_MACROS == "ALVEO_BOARD" } {
   # Setting destination offset for "TO"-only partition borders
   if { ($::env(PITON_TO_X) != $::env(PITON_X_TILES)-1 && $::env(PITON_TOX_PORT) == 0) &&
        ($::env(PITON_FR_X) == 0                       || $::env(PITON_FRX_PORT) != 0) } {
-    set CMAC_BRDG_DEST_OFFS [expr {$::env(PITON_Y_TILES) * 2}]
+    set CMAC_BRDG_DEST_OFFS [expr {$::env(PITON_Y_TILES) * 3 * 2}]
   }
   if { ($::env(PITON_TO_Y) != $::env(PITON_Y_TILES)-1 && $::env(PITON_TOY_PORT) == 0) &&
        ($::env(PITON_FR_Y) == 0                       || $::env(PITON_FRY_PORT) != 0) } {
-    set CMAC_BRDG_DEST_OFFS [expr {$::env(PITON_X_TILES) * 2}]
+    set CMAC_BRDG_DEST_OFFS [expr {$::env(PITON_X_TILES) * 3 * 2}]
   }
   # NOC_DATA_WIDTH/8 = 64/8 = 8
   set QSFP_BRDG_CHAN_BYTES 8
